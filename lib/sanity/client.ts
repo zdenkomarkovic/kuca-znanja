44ecd26ea5f9508239e27a005634508d57a46da8
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-08-16 14:43:08 +0200

    .

--- lib/sanity/client.ts
+++ lib/sanity/client.ts
@@ -4,7 +4,7 @@ export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "demo-project",
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   apiVersion: "2025-01-01", // koristi današnji datum
-  useCdn: true, // true za production
+  useCdn: false, // Privremeno false da rešimo cache problem
   perspective: "published",
 });
 
