8dbfcf735c83e017da91b545c43928a2ef6fb11e
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2026-01-21 10:27:35 +0100

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-import ButtonToTop from "@/components/ButtonToTop";
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
+// import ButtonToTop from "@/components/ButtonToTop";
+// import Footer from "@/components/Footer";
+// import Header from "@/components/Header";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -17,41 +17,45 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Kuća znanja – partner porodici u obrazovanju i vaspitanju",
-  description: "Kuća znanja na Vračaru – sigurno i kreativno okruženje za decu od 7 do 15 godina. Podrška u učenju, jezici, umetnost i razvoj kreativnosti.",
+  description:
+    "Kuća znanja na Vračaru – sigurno i kreativno okruženje za decu od 7 do 15 godina. Podrška u učenju, jezici, umetnost i razvoj kreativnosti.",
   icons: {
     icon: "/android-chrome-192x192.png",
     apple: "/apple-touch-icon.png",
   },
-  keywords: ["kuća znanja vračar", 
- "kuca znanja beograd", 
- "kreativno obrazovanje dece", 
- "produženi boravak vračar", 
- "produženi boravak beograd", 
- "učenje za decu beograd", 
- "kreativne radionice za decu", 
- "razvoj kreativnosti kod dece", 
- "škola za decu 7-15 godina", 
- "kreativno dete", 
- "obrazovanje dece vračar"],
+  keywords: [
+    "kuća znanja vračar",
+    "kuca znanja beograd",
+    "kreativno obrazovanje dece",
+    "produženi boravak vračar",
+    "produženi boravak beograd",
+    "učenje za decu beograd",
+    "kreativne radionice za decu",
+    "razvoj kreativnosti kod dece",
+    "škola za decu 7-15 godina",
+    "kreativno dete",
+    "obrazovanje dece vračar",
+  ],
   alternates: {
     canonical: "https://www.kucaznanja.edu.rs",
   },
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout() {
+//   {
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>
   return (
     <html lang="sr">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased text-muted-foreground bg-gradient-to-br from-white to-[#f1f1f1] text-base md:text-xl`}
       >
-        <Header />
+        {/* <Header />
         {children}
         <ButtonToTop />
-        <Footer />
+        <Footer /> */}
       </body>
     </html>
   );
